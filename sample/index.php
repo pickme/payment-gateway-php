2d0a66bba984e6c61d196581cbc95d88f3ac778a
Ronnachate <ronnachate@gamil.com>
2015-01-06 20:54:12 +0700

    Change params calling for doPayment function.

--- sample/index.php
+++ sample/index.php
@@ -4,42 +4,44 @@ require_once('../autoload.php');
 use Payment\Gateway;
 
 
-
 $config = array(
     'paypal' => array(
         'logfile' => '/tmp/paypal.log',
         'clientid' => 'AXWCshAD1aAEe3rmhqVBJU8jjl8NZvDP4sWE5utK6F30-jB80BTNWmnN2IvL',
         'clientSecret' => 'EJs8yRDtVU30otbO1olx20UqhCJK8X13kekGKVrUZGhwtqAoPc_jRLqRunLK',
-    	'sandbox' => '1'
+    	'sandboxmode' => '1'
      ),
     'braintree' => array(
         'merchantid' => '6wdpvcgyx8mrh9xs',
         'publickey' => 'rjzshvxbf2hdszdn',
         'privatekey' => '5a493c3625a3cf7b698b87fa1e68b223',
-    	'sandbox' => '1'
+    	'sandboxmode' => '1'
      )
 );
 
-
 $parms = array(
-    'CardInfo' => array(
-        'Test' => 'Test',
+    'cardinfo' => array(
+        'holder' => 'Test',
+    	'number' => '5105105105105100',
+    	'expired' => '05/12'
      ),
-    'Items' => array(
+    'items' => array(
         'id' => 'id',
      ),
-    'Currency' => 'USD'
+	'amount' => array(
+		'total' => '1000.00',
+		'currency' => 'USD',
+		'description' => 'Test'
+	)
 );
-
 
 $gateway = new Gateway($config);
 
 $provider = $gateway->getBraintreeProvider();
 
 $result = $provider->doPayment($parms);
-
-if($result->isSuccess ) {
-	print "Transaction success with id".$result->getReferenceId;
-}
+ if($result->isSuccess() ) {
+ 	print "Transaction success with id".$result->getReferenceId();
+ }
 
 echo "Success";
