8574039a229ec13def844043cb182e05aa17a621
Ronnachate <yo@abctech-thailand.com>
2015-01-07 20:12:30 +0700

    Add more stash function to mvc core.

--- sample/includes/Core.php
+++ sample/includes/Core.php
@@ -6,19 +6,32 @@ Class Core {
  * @the models array
  * @access private
  */
-    private $models = array();
+    private $_models = array();
+    private $_stash = array();
 
     public function model($name)
     {
- 	    if( !isset($this->models[$name]) ) {
+ 	    if( !isset($this->_models[$name]) ) {
      	    $modelName = "\\Payment\\Sample\\Model\\".$name;
      	
      	    include ('model/'.$name.'.php');
-     	    $this->models[$name] = new $modelName();
+     	    $this->_models[$name] = new $modelName();
         }
-        return $this->models[$name];
+        return $this->_models[$name];
     }
-
+    public function setStash($key, $val)
+    {
+    	$this->_models[$key] = $val;
+    }
+    public function getStash($val)
+    {
+    	if(isset($val)) {
+    		return $this->_stash[$val];
+    	}
+    	else {
+    		return $this->_stash;
+    	}
+    }
 }
 
 ?>
