66b9a635deadd2776167fe8b25d9f7332bf60878
Ronnachate <yo@abctech-thailand.com>
2015-01-09 11:18:33 +0700

    Render input form with Error message when do payment fialed.

--- sample/controller/Root.php
+++ sample/controller/Root.php
@@ -3,17 +3,59 @@
 
 class Root {
 	
-	private $_c = null;
+	private $c = null;
 
 	public function __construct($c)
 	{
-	    $this->_c = $c;
+	    $this->c = $c;
 	    
 	}
 	
 	public function index()
 	{
-	    $this->_c->setStash('User','Yo');
-	    $this->_c->renderView('Root');
+	    $this->c->setStash('User','Yo');
+	    $this->c->renderView('Root');
 	}
+
+    public function process()
+    {
+    	$result = null;
+        $params = $this->_getPaymentParams($this->c->requestParams());
+        
+        try 
+        {
+            $result = $this->c->model('PaymentGateway')->doPayment($params);
+        if($result->isSuccess() ) {
+            print "Transaction success with id".$result->getReferenceId();
+        }
+        else {
+            print "Transaction failed with msg ".$result->getErrorMsg();
+        }
+        }
+        catch ( \Exception $ex ) 
+        {
+            $this->c->setStash("ErrMsg",$ex->getMessage());
+        	$this->c->renderView('Root');
+        }
+        echo "Finish";
+    }
+
+    private function _getPaymentParams($val)
+    {
+        return array(
+            'payer' => array(
+                'cardinfo' => array(
+                    'type'   => $val['card_type'],
+                    'holder' => $val['holder'],
+                    'number' => $val['number'],
+                    'expired' => $val['mm'].'/'.$val['yy']
+                ),
+            ),
+            'amount' => array(
+                'total' => $val['total'],
+                'currency' => $val['currency'],
+                'description' => 'Descriptions'
+            )
+        );
+    }
 }
